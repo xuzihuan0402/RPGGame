7e24918b4f664f9d46d8a22b708551024b4bb922
miia <miia@com.tw>
2019-12-24 16:20:05 +0800

    12/24/0

---
+++ app/src/main/java/com/tom/game/GameView.java
@@ -0,0 +1,53 @@
+package com.tom.game;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class GameView extends View {
+    private float posX,posY=400;
+    private int lionWidth;
+    private int lionHeight;
+
+
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        canvas.drawLine(400,0,0,600,paint);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.lion);
+        canvas.drawBitmap(bitmap,posX,posY,paint);
+        lionWidth = bitmap.getWidth();
+        lionHeight = bitmap.getHeight();
+    }
+
+    public float getPosX() {
+        return posX;
+    }
+
+    public float getPosY() {
+        return posY;
+    }
+
+    public void setPosX(float posX) {
+        if(posX > 0 && posX < getWidth()-lionWidth){
+        this.posX = posX;
+        }
+    }
+
+    public void setPosY(float posY) {
+        if(posY > 0 && posY < getHeight()-lionHeight) {
+            this.posY = posY;
+        }
+    }
+}
--- app/src/main/java/com/tom/game/MainActivity.java
+++ app/src/main/java/com/tom/game/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private GameView gameView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,23 +23,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.arrow_down).setOnClickListener(this);
         findViewById(R.id.arrow_right).setOnClickListener(this);
         findViewById(R.id.arrow_left).setOnClickListener(this);
+        gameView = findViewById(R.id.Game);
     }
-
-
-    @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.arrow_up:
                 Log.d("MainActivity","onClick: UP");
+                gameView.setPosY(gameView.getPosY()-50);
+                gameView.invalidate();
                 break;
             case R.id.arrow_down:
                 Log.d("MainActivity","onClick: DOWN");
+                gameView.setPosY(gameView.getPosY()+50);
+                gameView.invalidate();
                 break;
             case R.id.arrow_left:
                 Log.d("MainActivity","onClick: LEFT");
+                gameView.setPosX(gameView.getPosX()-50);
+                gameView.invalidate();
                 break;
             case R.id.arrow_right:
                 Log.d("MainActivity","onClick: RIGTH");
+                gameView.setPosX(gameView.getPosX()+50);
+                gameView.invalidate();
                 break;
         }
     }
